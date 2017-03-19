67e222951fbfbea7401a4efc5246c6aa81d633d2
Nizar Aouissaoui <aouissaoui.nizar@gmail.com>
2017-03-19 15:09:15 +0100

    add angular material modify index add style css add index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './home/index';
+
+// MATERIAL DESIGN MODULES
+import { MaterialModule } from '@angular/material';
+
+import { APP_ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -14,7 +20,10 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule,
+    MaterialModule.forRoot(),
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
